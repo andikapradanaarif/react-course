419763da3df622d29cbc25da92bc8114aa81df19
andikaarif <andikapradanaarif@gmail.com>
2020-09-19 19:51:48 +0700

    fix : add rules eslintrc, change helmet to async

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <title>Page Title</title>
-      </Helmet>
-      <div className="App">
-        <h1>My First Heading</h1>
-        <p>My first paragraph.</p>
-      </div>
-    </>
+    <div className="App">
+      <h1>My First Heading</h1>
+      <p>My first paragraph.</p>
+    </div>
   );
 }
 
